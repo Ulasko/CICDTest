21db6d49591609620c906ac268ac981b23ff41c6
Ulasko <Ulasko1995@gmail.com>
2022-08-30 15:36:55 +0300

    Only Simple test

--- src/test/java/AuthorizationTests.java
+++ src/test/java/AuthorizationTests.java
@@ -3,13 +3,13 @@ import org.testng.annotations.Test;
 
 public class AuthorizationTests extends BaseTest {
 
-    @Test (description = " Новости дня - Газета.Ru ")
-    public void Authorization() {
-        new LogOnPage().goToLogOnPage()
-                .ClickGoBtn()
-                .CheckTitle();
-
-    }
+//    @Test (description = " Новости дня - Газета.Ru ")
+//    public void Authorization() {
+//        new LogOnPage().goToLogOnPage()
+//                .ClickGoBtn()
+//                .CheckTitle();
+//
+//    }
     @Test (description = " Новости")
     public void Simple() {
         Assert.assertEquals("login", "login");
